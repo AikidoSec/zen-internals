f8bec839d6118946bc6580df6383692caf55847b
Wout Feys <wout@aikido.dev>
2024-09-19 18:23:00 +0200

    Create new path_traversal module

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 /* Zen Vulnerability library, contains Zen vulnerability code written in Rust and exported
  * Using FFI. Currently we support the following algorithms :
- * - Shell Injection (WIP)
+ * - Shell Injection
+ * - Path Traversal (WIP)
  */
+pub mod path_traversal;
 pub mod shell_injection;
