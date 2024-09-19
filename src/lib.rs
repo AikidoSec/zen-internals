af89d3d94d37f74942b4b69a7c78d1c4ae5f53d4
Wout Feys <wout@aikido.dev>
2024-09-19 17:22:33 +0200

    Add shell injection source code

---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+/* Zen Vulnerability library, contains Zen vulnerability code written in Rust and exported
+ * Using FFI. Currently we support the following algorithms : 
+ * - Shell Injection (WIP)
+ */
+pub mod shell_injection;
