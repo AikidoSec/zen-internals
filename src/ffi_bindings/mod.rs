196055defae020c479734bfcd1507d726798e5ca
Wout Feys <wout@aikido.dev>
2024-09-20 10:33:54 +0200

    Move detect_shell_injection FFI function to ffi_bindings

--- src/ffi_bindings/mod.rs
+++ src/ffi_bindings/mod.rs
@@ -1,6 +1,7 @@
 use crate::route_builder::build_route_from_url::build_route_from_url_str;
+use crate::shell_injection::detect_shell_injection::detect_shell_injection_stringified;
 use std::ffi::{CStr, CString};
-use std::os::raw::c_char;
+use std::os::raw::{c_char, c_int};
 use std::str;
 
 #[no_mangle]
@@ -16,4 +17,25 @@ pub extern "C" fn build_route_from_url(url: *const c_char) -> *mut c_char {
     let route = build_route_from_url_str(url_str).unwrap_or("".to_string());
     let c_string = CString::new(route).expect("CString::new failed");
     c_string.into_raw()
+}
+
+#[no_mangle]
+pub extern "C" fn detect_shell_injection(
+    command: *const c_char,
+    userinput: *const c_char,
+) -> c_int {
+    // Check if the pointers are null
+    if command.is_null() || userinput.is_null() {
+        eprintln!("Received null pointer for command or userinput.");
+        return 0; // Handle the error as needed
+    }
+    let command_bytes = unsafe { CStr::from_ptr(command).to_bytes() };
+    let userinput_bytes = unsafe { CStr::from_ptr(userinput).to_bytes() };
+
+    let command_str = str::from_utf8(command_bytes).unwrap();
+    let userinput_str = str::from_utf8(userinput_bytes).unwrap();
+    if detect_shell_injection_stringified(command_str, userinput_str) {
+        return 1;
+    }
+    0
 }
--- src/shell_injection/detect_shell_injection.rs
+++ src/shell_injection/detect_shell_injection.rs
@@ -1,29 +1,5 @@
 use super::contains_shell_syntax::contains_shell_syntax;
 use super::is_safely_encapsulated::is_safely_encapsulated;
-use std::ffi::CStr;
-use std::os::raw::{c_char, c_int};
-use std::str;
-
-#[no_mangle]
-pub extern "C" fn detect_shell_injection(
-    command: *const c_char,
-    userinput: *const c_char,
-) -> c_int {
-    // Check if the pointers are null
-    if command.is_null() || userinput.is_null() {
-        eprintln!("Received null pointer for command or userinput.");
-        return 0; // Handle the error as needed
-    }
-    let command_bytes = unsafe { CStr::from_ptr(command).to_bytes() };
-    let userinput_bytes = unsafe { CStr::from_ptr(userinput).to_bytes() };
-
-    let command_str = str::from_utf8(command_bytes).unwrap();
-    let userinput_str = str::from_utf8(userinput_bytes).unwrap();
-    if detect_shell_injection_stringified(command_str, userinput_str) {
-        return 1;
-    }
-    0
-}
 
 pub fn detect_shell_injection_stringified(command: &str, user_input: &str) -> bool {
     if user_input == "~" && command.len() > 1 && command.contains("~") {
