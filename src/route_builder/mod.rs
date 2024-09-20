adf0b1df59b4ec521dbee252caa55460dd1f4877
Wout Feys <wout@aikido.dev>
2024-09-20 09:29:07 +0200

    Add build_route_from_url_str function

--- src/route_builder/mod.rs
+++ src/route_builder/mod.rs
@@ -3,3 +3,6 @@ pub mod looks_like_a_secret_test;
 
 pub mod replace_segment_with_param;
 pub mod replace_segment_with_param_test;
+
+pub mod build_route_from_url;
+pub mod build_route_from_url_test;
