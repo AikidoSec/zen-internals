ba217552ed49bf89f9065de5728a4b561e27ee4c
Wout Feys <wout@aikido.dev>
2024-09-23 12:03:15 +0200

    Update is_dangerous_token and add unit tests

--- src/sql_injection/is_dangerous_token.rs
+++ src/sql_injection/is_dangerous_token.rs
@@ -1,58 +1,33 @@
-use sqlparser::ast::DollarQuotedString;
-use sqlparser::keywords::Keyword;
 use sqlparser::tokenizer::*;
 
-// Safe string tokens :
-// https://github.com/sqlparser-rs/sqlparser-rs/blob/affe8b549884a351ead4f35aa8bdf4cae8c93e4b/src/tokenizer.rs#L65C1-L105C30
-const SAFE_STRING_TOKENS: [Token; 17] = [
-    Token::SingleQuotedString(String::new()), // Placeholder, as we need a String
-    Token::DoubleQuotedString(String::new()), // Placeholder, as we need a String
-    Token::TripleSingleQuotedString(String::new()), // Triple single quoted strings
-    Token::TripleDoubleQuotedString(String::new()), // Triple double quoted strings
-    Token::DollarQuotedString(DollarQuotedString {
-        value: String::new(),
-        tag: None,
-    }), // Dollar quoted string
-    Token::SingleQuotedByteStringLiteral(String::new()), // Byte string literal
-    Token::DoubleQuotedByteStringLiteral(String::new()), // Byte string literal
-    Token::TripleSingleQuotedByteStringLiteral(String::new()), // Triple single quoted byte string
-    Token::TripleDoubleQuotedByteStringLiteral(String::new()), // Triple double quoted byte string
-    Token::SingleQuotedRawStringLiteral(String::new()), // Single quoted raw string
-    Token::DoubleQuotedRawStringLiteral(String::new()), // Double quoted raw string
-    Token::TripleSingleQuotedRawStringLiteral(String::new()), // Triple single quoted raw string
-    Token::TripleDoubleQuotedRawStringLiteral(String::new()), // Triple double quoted raw string
-    Token::NationalStringLiteral(String::new()), // National string literal
-    Token::EscapedStringLiteral(String::new()), // Escaped string literal
-    Token::UnicodeStringLiteral(String::new()), // Unicode string literal
-    Token::HexStringLiteral(String::new()),   // Hexadecimal string literal
-];
+pub fn is_dangerous_token(token: &Token) -> bool {
+    match token {
+        // Safe string tokens :
+        // https://github.com/sqlparser-rs/sqlparser-rs/blob/affe8b549884a351ead4f35aa8bdf4cae8c93e4b/src/tokenizer.rs#L65C1-L105C30
+        Token::SingleQuotedString(_) => false,
+        Token::DoubleQuotedString(_) => false,
+        Token::TripleSingleQuotedString(_) => false,
+        Token::TripleDoubleQuotedString(_) => false,
+        Token::DollarQuotedString(_) => false,
+        Token::SingleQuotedByteStringLiteral(_) => false,
+        Token::DoubleQuotedByteStringLiteral(_) => false,
+        Token::TripleSingleQuotedByteStringLiteral(_) => false,
+        Token::TripleDoubleQuotedByteStringLiteral(_) => false,
+        Token::SingleQuotedRawStringLiteral(_) => false,
+        Token::DoubleQuotedRawStringLiteral(_) => false,
+        Token::TripleSingleQuotedRawStringLiteral(_) => false,
+        Token::TripleDoubleQuotedRawStringLiteral(_) => false,
+        Token::NationalStringLiteral(_) => false,
+        Token::EscapedStringLiteral(_) => false,
+        Token::UnicodeStringLiteral(_) => false,
+        Token::HexStringLiteral(_) => false,
 
-// General safe tokens :
-const GENERAL_SAFE_TOKENS: [Token; 3] = [
-    Token::EOF, // An end-of-file marker, not a real token
-    Token::Word(Word {
-        value: String::new(),
-        quote_style: None,
-        keyword: Keyword::DEFAULT,
-    }), // A keyword (like SELECT) or an optionally quoted SQL identifier
-    Token::Whitespace(Whitespace::MultiLineComment(String::new())), // Whitespace (space, tab, etc)
-];
+        // General safe tokens
+        Token::EOF => false,
+        Token::Word(_) => false,
+        Token::Whitespace(_) => false,
 
-pub fn is_dangerous_token(token: Token) -> bool {
-    // Check if the token is in the safe string tokens
-    for safe_token in &SAFE_STRING_TOKENS {
-        if &token == safe_token {
-            return false; // Not dangerous
-        }
+        // All other tokens are considered dangerous
+        _ => true,
     }
-
-    // Check if the token is in the general safe tokens
-    for general_safe_token in &GENERAL_SAFE_TOKENS {
-        if &token == general_safe_token {
-            return false; // Not dangerous
-        }
-    }
-
-    // If the token is not in either safe list, it is considered dangerous
-    true
 }
--- src/sql_injection/is_dangerous_token_test.rs
+++ src/sql_injection/is_dangerous_token_test.rs
@@ -1,1 +1,125 @@
+#[cfg(test)]
+mod tests {
+    use crate::sql_injection::is_dangerous_token::is_dangerous_token;
+    use sqlparser::ast::DollarQuotedString;
+    use sqlparser::keywords::Keyword;
+    use sqlparser::tokenizer::{Token, Whitespace, Word};
 
+    #[test]
+    fn test_safe_tokens() {
+        // Test various safe tokens
+        assert!(!is_dangerous_token(&Token::SingleQuotedString(
+            "test".to_string()
+        )));
+        assert!(!is_dangerous_token(&Token::DoubleQuotedString(
+            "test".to_string()
+        )));
+        assert!(!is_dangerous_token(&Token::TripleSingleQuotedString(
+            "test".to_string()
+        )));
+        assert!(!is_dangerous_token(&Token::TripleDoubleQuotedString(
+            "test".to_string()
+        )));
+        assert!(!is_dangerous_token(&Token::DollarQuotedString(
+            DollarQuotedString {
+                value: "test".to_string(),
+                tag: None,
+            }
+        )));
+        assert!(!is_dangerous_token(&Token::SingleQuotedByteStringLiteral(
+            "test".to_string()
+        )));
+        assert!(!is_dangerous_token(&Token::DoubleQuotedByteStringLiteral(
+            "test".to_string()
+        )));
+        assert!(!is_dangerous_token(
+            &Token::TripleSingleQuotedByteStringLiteral("test".to_string())
+        ));
+        assert!(!is_dangerous_token(
+            &Token::TripleDoubleQuotedByteStringLiteral("test".to_string())
+        ));
+        assert!(!is_dangerous_token(&Token::SingleQuotedRawStringLiteral(
+            "test".to_string()
+        )));
+        assert!(!is_dangerous_token(&Token::DoubleQuotedRawStringLiteral(
+            "test".to_string()
+        )));
+        assert!(!is_dangerous_token(
+            &Token::TripleSingleQuotedRawStringLiteral("test".to_string())
+        ));
+        assert!(!is_dangerous_token(
+            &Token::TripleDoubleQuotedRawStringLiteral("test".to_string())
+        ));
+        assert!(!is_dangerous_token(&Token::NationalStringLiteral(
+            "test".to_string()
+        )));
+        assert!(!is_dangerous_token(&Token::EscapedStringLiteral(
+            "test".to_string()
+        )));
+        assert!(!is_dangerous_token(&Token::UnicodeStringLiteral(
+            "test".to_string()
+        )));
+        assert!(!is_dangerous_token(&Token::HexStringLiteral(
+            "test".to_string()
+        )));
+    }
+
+    #[test]
+    fn test_general_safe_tokens() {
+        // Test general safe tokens
+        assert!(!is_dangerous_token(&Token::EOF));
+        assert!(!is_dangerous_token(&Token::Word(Word {
+            value: "SELECT".to_string(),
+            quote_style: None,
+            keyword: Keyword::SELECT,
+        })));
+        assert!(!is_dangerous_token(&Token::Whitespace(
+            Whitespace::SingleLineComment {
+                comment: "test".to_string(),
+                prefix: "".to_string()
+            }
+        )));
+    }
+
+    #[test]
+    fn test_dangerous_tokens() {
+        // Test various dangerous tokens
+        assert!(is_dangerous_token(&Token::Number("123".to_string(), true))); // Unsigned numeric literal
+        assert!(is_dangerous_token(&Token::Char('a'))); // Unrecognized character
+        assert!(is_dangerous_token(&Token::Comma)); // Comma token
+        assert!(is_dangerous_token(&Token::DoubleEq)); // Double equals sign
+        assert!(is_dangerous_token(&Token::Eq)); // Equality operator
+        assert!(is_dangerous_token(&Token::Neq)); // Not equals operator
+        assert!(is_dangerous_token(&Token::Lt)); // Less than operator
+        assert!(is_dangerous_token(&Token::Gt)); // Greater than operator
+        assert!(is_dangerous_token(&Token::LtEq)); // Less than or equals operator
+        assert!(is_dangerous_token(&Token::GtEq)); // Greater than or equals operator
+        assert!(is_dangerous_token(&Token::Plus)); // Plus operator
+        assert!(is_dangerous_token(&Token::Minus)); // Minus operator
+        assert!(is_dangerous_token(&Token::Mul)); // Multiplication operator
+        assert!(is_dangerous_token(&Token::Div)); // Division operator
+        assert!(is_dangerous_token(&Token::Mod)); // Modulo operator
+        assert!(is_dangerous_token(&Token::StringConcat)); // String concatenation operator
+        assert!(is_dangerous_token(&Token::LParen)); // Left parenthesis
+        assert!(is_dangerous_token(&Token::RParen)); // Right parenthesis
+        assert!(is_dangerous_token(&Token::Period)); // Period token
+        assert!(is_dangerous_token(&Token::SemiColon)); // Semicolon token
+        assert!(is_dangerous_token(&Token::Backslash)); // Backslash token
+        assert!(is_dangerous_token(&Token::Ampersand)); // Ampersand token
+        assert!(is_dangerous_token(&Token::Pipe)); // Pipe token
+        assert!(is_dangerous_token(&Token::Caret)); // Caret token
+        assert!(is_dangerous_token(&Token::AtSign)); // At sign token
+        assert!(is_dangerous_token(&Token::Placeholder(
+            "placeholder".to_string()
+        ))); // Placeholder token
+        assert!(is_dangerous_token(&Token::Arrow)); // Arrow operator
+        assert!(is_dangerous_token(&Token::LongArrow)); // Long arrow operator
+        assert!(is_dangerous_token(&Token::HashArrow)); // Hash arrow operator
+        assert!(is_dangerous_token(&Token::HashLongArrow)); // Hash long arrow operator
+        assert!(is_dangerous_token(&Token::AtQuestion)); // JSON path question operator
+        assert!(is_dangerous_token(&Token::Question)); // JSON key existence operator
+        assert!(is_dangerous_token(&Token::CustomBinaryOperator(
+            "custom_op".to_string()
+        ))); // Custom binary operator
+    }
+}
