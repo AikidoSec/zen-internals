962808b3e2ac8e599d618d9e9d89ce94ff689dd2
Wout Feys <wout@aikido.dev>
2024-09-23 14:42:35 +0200

    Set keywords as safe but flag every word that is not a keyword

--- src/sql_injection/is_dangerous_token.rs
+++ src/sql_injection/is_dangerous_token.rs
@@ -1,6 +1,14 @@
+use sqlparser::keywords::Keyword;
 use sqlparser::tokenizer::*;
-
 pub fn is_dangerous_token(token: &Token) -> bool {
+    // Check if token is "word" so we can check against safe keywords list :
+    if let Token::Word(word) = token {
+        // Return true if the word is not a Keyword, this is marked as dangerous.
+        return match word.keyword {
+            Keyword::NoKeyword => true,
+            _ => false,
+        };
+    }
     match token {
         // Safe string tokens :
         // https://github.com/sqlparser-rs/sqlparser-rs/blob/affe8b549884a351ead4f35aa8bdf4cae8c93e4b/src/tokenizer.rs#L65C1-L105C30
