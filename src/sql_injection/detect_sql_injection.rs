2976d0a47f39d148eb3b88f9499742a4791f3f0d
Wout Feys <wout@aikido.dev>
2024-09-23 13:15:24 +0200

    Create fallback if dialect is not generic
    Also returns false if tokenizer fails

--- src/sql_injection/detect_sql_injection.rs
+++ src/sql_injection/detect_sql_injection.rs
@@ -1,5 +1,6 @@
 use super::is_dangerous_token::is_dangerous_token;
 use super::tokenize_query::tokenize_query;
+use sqlparser::tokenizer::Token;
 
 pub fn detect_sql_injection_str(query: &str, userinput: &str, dialect: i32) -> bool {
     if userinput.len() <= 3 || query.len() < userinput.len() {
@@ -15,27 +16,39 @@ pub fn detect_sql_injection_str(query: &str, userinput: &str, dialect: i32) -> b
         // Userinput is not inside query, so not an injection.
         return false;
     }
-    // Count dangerous tokens in query :
-    let dangerous_tokens = count_dangerous_tokens(query_lower, dialect);
 
-    // Count dangerous tokens in query without user input :
+    // Tokenize query :
+    let tokens = tokenize_with_fallback(query, dialect);
+    if tokens.len() <= 0 {
+        // Tokens are empty, probably a parsing issue, return false.
+        return false;
+    }
+    // Tokenize query without user input :
     // We replace with "COUNT" since this is a Word and matched as a safe token.
     let query_without_input: &str = &query_lower.replace(userinput_lower, "COUNT");
-    let dangerous_tokens_without_input = count_dangerous_tokens(query_without_input, dialect);
+    let tokens_without_input = tokenize_with_fallback(query_without_input, dialect);
 
-    println!(
-        "Dangerous Tokens with input : {}, Dangerous tokens without input : {}",
-        dangerous_tokens, dangerous_tokens_without_input
-    );
-    // If the amount of dangerous tokens does not match this probably means an injection took place.
-    dangerous_tokens != dangerous_tokens_without_input
+    return delta_dangerous_tokens(tokens, tokens_without_input) == 0;
 }
 
-fn count_dangerous_tokens(query: &str, dialect: i32) -> usize {
+fn tokenize_with_fallback(query: &str, dialect: i32) -> Vec<Token> {
     let query_tokens = tokenize_query(query, dialect);
-    let dangerous_tokens: Vec<_> = query_tokens
+    if query_tokens.len() <= 0 && dialect != 0 {
+        // Dialect is not generic and query_tokens are empty, make fallback
+        return tokenize_query(query, 0);
+    }
+    return query_tokens;
+}
+
+fn delta_dangerous_tokens(tokens1: Vec<Token>, tokens2: Vec<Token>) -> usize {
+    let dangerous_tokens1: Vec<_> = tokens1
         .iter()
         .filter(|&token| is_dangerous_token(token))
         .collect();
-    return dangerous_tokens.len();
+    let dangerous_tokens2: Vec<_> = tokens2
+        .iter()
+        .filter(|&token| is_dangerous_token(token))
+        .collect();
+
+    return dangerous_tokens2.len() - dangerous_tokens1.len();
 }
