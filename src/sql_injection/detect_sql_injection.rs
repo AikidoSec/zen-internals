5b57c2e9e305a5055354aa96cdb149bb7741e184
Wout Feys <wout@aikido.dev>
2024-09-24 17:14:36 +0200

    Update sql injection algorithm to count whitespace and general

--- src/sql_injection/detect_sql_injection.rs
+++ src/sql_injection/detect_sql_injection.rs
@@ -2,38 +2,33 @@ use super::is_whitespace_token::is_whitespace_token;
 use super::tokenize_query::tokenize_query;
 use sqlparser::tokenizer::Token;
 
+// `userinput` and `query` provided to this function should already be lowercase.
 pub fn detect_sql_injection_str(query: &str, userinput: &str, dialect: i32) -> bool {
-    if userinput.chars().all(char::is_alphanumeric) {
-        // Userinput is alphanumerical, ignore.
-        return false;
-    }
-    if userinput.len() <= 3 || query.len() < userinput.len() {
-        // Ignore small user input or a query that's too small.
-        return false;
-    }
-
-    // Lowercase both query and userinput
-    let query_lower: &str = &query.to_lowercase();
-    let userinput_lower: &str = &userinput.to_lowercase();
-
-    if !query_lower.contains(userinput_lower) {
-        // Userinput is not inside query, so not an injection.
-        return false;
-    }
-
     // Tokenize query :
     let tokens = tokenize_with_fallback(query, dialect);
     if tokens.len() <= 0 {
-        // Tokens are empty, probably a parsing issue, return false.
+        // Tokens are empty, probably a parsing issue with original query, return false.
         return false;
     }
+
     // Tokenize query without user input :
-    // We replace with "COUNT" since this is a Word and matched as a safe token.
-    let query_without_input: &str = &query_lower.replace(userinput_lower, "COUNT");
+    let query_without_input: &str = &query.replace(userinput, "str");
     let tokens_without_input = tokenize_with_fallback(query_without_input, dialect);
 
-    // Return true if a delta exists, because this indicates a possible injection :
-    return delta_dangerous_tokens(tokens, tokens_without_input) != 0;
+    // Check delta for both whitespace tokens and all tokens in general :
+    let tokens_general_delta = tokens.len() - tokens_without_input.len();
+    if tokens_general_delta != 0 {
+        // If a delta exists in all tokens, mark this as an injection.
+        return true;
+    }
+    let tokens_whitespace_delta = delta_whitespace_tokens(tokens, tokens_without_input);
+    if tokens_whitespace_delta != 0 {
+        // A delta exists in whitespace tokens, regardless of input the amount of whitespace
+        // tokens, like comments, should remain the same.
+        return true;
+    }
+
+    return false;
 }
 
 fn tokenize_with_fallback(query: &str, dialect: i32) -> Vec<Token> {
@@ -45,14 +40,14 @@ fn tokenize_with_fallback(query: &str, dialect: i32) -> Vec<Token> {
     return query_tokens;
 }
 
-fn delta_dangerous_tokens(tokens1: Vec<Token>, tokens2: Vec<Token>) -> usize {
+fn delta_whitespace_tokens(tokens1: Vec<Token>, tokens2: Vec<Token>) -> usize {
     let dangerous_tokens1: Vec<_> = tokens1
         .iter()
-        .filter(|&token| is_dangerous_token(token))
+        .filter(|&token| is_whitespace_token(token))
         .collect();
     let dangerous_tokens2: Vec<_> = tokens2
         .iter()
-        .filter(|&token| is_dangerous_token(token))
+        .filter(|&token| is_whitespace_token(token))
         .collect();
 
     return dangerous_tokens2.len() - dangerous_tokens1.len();
