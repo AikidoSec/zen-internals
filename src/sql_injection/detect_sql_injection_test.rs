e9c2ffc81ef9ca519ac16db0e4d1e85081ee47a8
Wout Feys <wout@aikido.dev>
2024-09-25 12:53:48 +0200

    Add detect_sql_injection first test cases

--- src/sql_injection/detect_sql_injection_test.rs
+++ src/sql_injection/detect_sql_injection_test.rs
@@ -1,1 +1,14 @@
-
+#[cfg(test)]
+mod tests {
+    use crate::sql_injection::detect_sql_injection::detect_sql_injection_str;
+    #[test]
+    fn test_escaping_strings() {
+        assert!(detect_sql_injection_str(
+            "INSERT INTO users (name, age, email, city) VALUES ('Alice');;;;;;;    -- ', 30, 'alice@example.com', 'Wonderland'); -- Hellow"
+            ,
+            "Alice');;;;;;;    --", 0));
+        assert!(detect_sql_injection_str(
+            "INSERT INTO users (name, age, email, city) VALUES ('Alice');;;;;;;  -- ', 30, 'alice@example.com', 'Wonderland');",
+            "Alice');;;;;;;  --", 0));
+    }
+}
