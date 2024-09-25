f8f0b2125892e2bea1a3cabb1a7237f64538abd5
Wout Feys <wout@aikido.dev>
2024-09-25 14:39:28 +0200

    Use macros to check if it is an injection or not

--- src/sql_injection/detect_sql_injection_test.rs
+++ src/sql_injection/detect_sql_injection_test.rs
@@ -1,122 +1,112 @@
+macro_rules! is_injection {
+    ($query:expr, $input:expr) => {
+        assert!(detect_sql_injection_str($query, $input, 0))
+    };
+}
+macro_rules! not_is_injection {
+    ($query:expr, $input:expr) => {
+        assert!(!detect_sql_injection_str($query, $input, 0))
+    };
+}
 #[cfg(test)]
 mod tests {
     use crate::sql_injection::detect_sql_injection::detect_sql_injection_str;
+
     #[test]
     fn test_injection_with_token_counts() {
-        // SQL Injection :
-        assert!(detect_sql_injection_str(
+        is_injection!(
             "INSERT INTO users (name, age, email, city) VALUES ('Alice');;;;;;;    -- ', 30, 'alice@example.com', 'Wonderland'); -- Hellow",
-            "Alice');;;;;;;    --", 0));
-        assert!(detect_sql_injection_str(
+            "Alice');;;;;;;    --"
+        );
+        is_injection!(
             "INSERT INTO users (name, age, email, city) VALUES ('Alice');;;;;;;  -- ', 30, 'alice@example.com', 'Wonderland');",
-            "Alice');;;;;;;  --", 0));
+            "Alice');;;;;;;  --"
+        );
     }
 
     #[test]
     fn test_encapsulated_strings() {
-        // Not a SQL Injection :
-        assert!(!detect_sql_injection_str(
+        not_is_injection!(
             "INSERT INTO users (name, surname) VALUES ('Alice', 'Bob')",
-            "Alice",
-            0
-        ));
-        assert!(!detect_sql_injection_str(
+            "Alice"
+        );
+        not_is_injection!(
             "INSERT INTO users (name, surname) VALUES ('Alice', 'Bob')",
-            "Bob",
-            0
-        ));
-        assert!(!detect_sql_injection_str(
+            "Bob"
+        );
+        not_is_injection!(
             "INSERT INTO users (name, surname) VALUES ('W.Al294*is', 'Bob')",
-            "W.Al294*is",
-            0
-        ));
-        assert!(!detect_sql_injection_str(
+            "W.Al294*is"
+        );
+        not_is_injection!(
             "INSERT INTO users (name, surname) VALUES ('John', 'Doe')",
-            "'John'",
-            0
-        ));
+            "'John'"
+        );
 
-        // SQL Injection :
-        assert!(detect_sql_injection_str(
+        is_injection!(
             "INSERT INTO users (name, surname) VALUES ('John', 'Doe')",
-            "John', 'Doe",
-            0
-        ));
-        assert!(detect_sql_injection_str(
+            "John', 'Doe"
+        );
+        is_injection!(
             "INSERT INTO users (name, surname) VALUES ('John', 'Doe')",
-            "John',",
-            0
-        ));
+            "John',"
+        );
     }
 
     #[test]
     fn test_keywords() {
-        // Not a SQL Injection :
-        assert!(!detect_sql_injection_str(
+        not_is_injection!(
             "INSERT INTO users (name, surname) VALUES ('Alice', 'Bob')",
-            "INSERT",
-            0
-        ));
-        assert!(!detect_sql_injection_str(
+            "INSERT"
+        );
+        not_is_injection!(
             "INSERT INTO users (name, surname) VALUES ('Alice', 'Bob')",
-            "INS",
-            0
-        ));
-        assert!(!detect_sql_injection_str(
+            "INS"
+        );
+        not_is_injection!(
             "INSERT INTO users (name, surname) VALUES ('Alice', 'Bob')",
-            "users",
-            0
-        ));
-        assert!(!detect_sql_injection_str(
+            "users"
+        );
+        not_is_injection!(
             "INSERT INTO users (name, surname) VALUES ('Alice', 'Bob')",
-            "VALUES",
-            0
-        ));
+            "VALUES"
+        );
 
-        // SQL Injection :
-        assert!(detect_sql_injection_str(
+        is_injection!(
             "INSERT INTO users (name, surname) VALUES ('Alice', 'Bob')",
-            "INSERT INTO",
-            0
-        ));
-        assert!(detect_sql_injection_str(
+            "INSERT INTO"
+        );
+        is_injection!(
             "INSERT INTO users (name, surname) VALUES ('Alice', 'Bob')",
-            "INSERT INTO users",
-            0
-        ));
-        assert!(detect_sql_injection_str(
+            "INSERT INTO users"
+        );
+        is_injection!(
             "INSERT INTO users (name, surname) VALUES ('Alice', 'Bob')",
-            "INTO users",
-            0
-        ));
-        assert!(detect_sql_injection_str(
+            "INTO users"
+        );
+        is_injection!(
             "INSERT INTO users (name, surname) VALUES ('Alice', 'Bob')",
-            "VALUES ",
-            0
-        ));
-        assert!(detect_sql_injection_str(
+            "VALUES "
+        );
+        is_injection!(
             "INSERT INTO users (name, surname) VALUES ('Alice', 'Bob')",
-            " INTO",
-            0
-        ));
+            " INTO"
+        );
     }
     #[test]
     fn test_character_combos() {
         // SQL Injection :
-        assert!(detect_sql_injection_str(
+        is_injection!(
             "INSERT INTO users (name, surname) VALUES ('Alice', 'Bob')",
-            "('",
-            0
-        ));
-        assert!(detect_sql_injection_str(
+            "('"
+        );
+        is_injection!(
             "INSERT INTO users (name, surname) VALUES ('Alice', 'Bob')",
-            ", ",
-            0
-        ));
-        assert!(detect_sql_injection_str(
+            ", "
+        );
+        is_injection!(
             "INSERT  INTO users (name, surname) VALUES ('Alice', 'Bob')",
-            "  ",
-            0
-        ));
+            "  "
+        );
     }
 }
