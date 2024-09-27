f9b6ec14986c3758131603436d05566485a41668
Wout Feys <wout@aikido.dev>
2024-09-27 11:20:42 +0200

    Remove route builder from wasm

--- src/wasm_bindings/mod.rs
+++ src/wasm_bindings/mod.rs
@@ -1,12 +1,6 @@
-use crate::route_builder::build_route_from_url::build_route_from_url_str;
 use crate::shell_injection::detect_shell_injection::detect_shell_injection_stringified;
 use crate::sql_injection::detect_sql_injection::detect_sql_injection_str;
 use wasm_bindgen::prelude::*;
-
-#[wasm_bindgen]
-pub fn wasm_build_route_from_url(url: &str) -> String {
-    build_route_from_url_str(url).unwrap_or("".to_string())
-}
 
 #[wasm_bindgen]
 pub fn wasm_detect_shell_injection(command: &str, userinput: &str) -> bool {
