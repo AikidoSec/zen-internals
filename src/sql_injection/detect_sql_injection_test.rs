7db6181787f50f6c577de7b7ea06dfe3ce0dc449
Hans Ott <hansott@hotmail.be>
2024-10-04 18:48:13 +0200

    Add tests for postgres dollar signs

--- src/sql_injection/detect_sql_injection_test.rs
+++ src/sql_injection/detect_sql_injection_test.rs
@@ -35,6 +35,38 @@ mod tests {
     use crate::sql_injection::detect_sql_injection::detect_sql_injection_str;
 
     #[test]
+    fn test_postgres_dollar_signs() {
+        not_is_injection!(
+            "SELECT * FROM users WHERE id = $$' OR 1=1 -- $$",
+            "' OR 1=1 -- "
+        );
+        not_is_injection!(
+            "SELECT * FROM users WHERE id = $$1; DROP TABLE users; -- $$",
+            "1; DROP TABLE users; -- "
+        );
+        is_injection!(
+            "SELECT * FROM users WHERE id = $$1$$ OR 1=1 -- $$",
+            "1$$ OR 1=1 -- "
+        );
+    }
+
+    #[test]
+    fn test_postgres_dollar_named_dollar_signs() {
+        not_is_injection!(
+            "SELECT * FROM users WHERE id = $name$' OR 1=1 -- $name$",
+            "' OR 1=1 -- "
+        );
+        not_is_injection!(
+            "SELECT * FROM users WHERE id = $name$1; DROP TABLE users; -- $name$",
+            "1; DROP TABLE users; -- "
+        );
+        is_injection!(
+            "SELECT * FROM users WHERE id = $name$1$name$ OR 1=1 -- $name$",
+            "1$name$ OR 1=1 -- "
+        );
+    }
+
+    #[test]
     fn test_it_detects_injections() {
         is_injection!(
             "SELECT * FROM users WHERE id = '' OR 1=1 -- '",
