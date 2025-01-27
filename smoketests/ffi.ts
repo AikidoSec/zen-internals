0a1fb3c5b0b8d5e868baefa57c5460dc4ed70c6d
Hans Ott <hansott@hotmail.be>
2025-01-27 13:25:03 +0100

    Add additional checks for FFI

--- smoketests/ffi.ts
+++ smoketests/ffi.ts
@@ -26,24 +26,54 @@ function toCString(str: string): Deno.PointerValue {
     return Deno.UnsafePointer.of(buffer);
 }
 
+// Test SQL injection
 assertEquals(
     lib.symbols.detect_sql_injection(toCString("SELECT * FROM users WHERE id = '' OR 1=1 -- '"), toCString("' OR 1=1 -- "), 0),
     1
 );
 
+// Not an injection
 assertEquals(
     lib.symbols.detect_sql_injection(toCString("SELECT * FROM users WHERE id = 'hello world'"), toCString("hello world"), 0),
     0
 );
 
+// Test fallback to generic dialect
+assertEquals(
+    lib.symbols.detect_sql_injection(toCString("SELECT * FROM users WHERE id = '' OR 1=1 -- '"), toCString("' OR 1=1 -- "), 2141),
+    1
+);
+
+// Test unsafe pointer
+assertEquals(
+    lib.symbols.detect_sql_injection(toCString("🔥"), Deno.UnsafePointer.of(new Uint8Array([])), 0),
+    2
+);
+assertEquals(
+    lib.symbols.detect_sql_injection(Deno.UnsafePointer.of(new Uint8Array([])), toCString("🔥"), 0),
+    2
+);
+
+// Test JS injection
 assertEquals(
     lib.symbols.detect_js_injection(toCString("const test = 'Hello World!'; //';"), toCString("Hello World!'; //"), 0),
     1
 );
 
+// Not an injection
 assertEquals(
     lib.symbols.detect_js_injection(toCString("const test = 'Hello World!';"), toCString("Hello World!"), 0),
     0
+);
+
+// Test unsafe pointer
+assertEquals(
+    lib.symbols.detect_js_injection(toCString("🔥"), Deno.UnsafePointer.of(new Uint8Array([])), 0),
+    2
+);
+assertEquals(
+    lib.symbols.detect_js_injection(Deno.UnsafePointer.of(new Uint8Array([])), toCString("🔥"), 0),
+    2
 );
 
 lib.close();
