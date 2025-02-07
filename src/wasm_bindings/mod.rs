8fef59c7c70c1d1b163ed1ebab368470844ca188
Hans Ott <hansott@hotmail.be>
2025-02-07 12:08:28 +0100

    Remove shell injection detection algo
    Let's add back when it's 100%

--- src/wasm_bindings/mod.rs
+++ src/wasm_bindings/mod.rs
@@ -4,11 +4,6 @@ use crate::sql_injection::detect_sql_injection::detect_sql_injection_str;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn wasm_detect_shell_injection(command: &str, userinput: &str) -> bool {
-    detect_shell_injection_stringified(command, userinput)
-}
-
-#[wasm_bindgen]
 pub fn wasm_detect_sql_injection(query: &str, userinput: &str, dialect: i32) -> bool {
     detect_sql_injection_str(query, userinput, dialect)
 }
