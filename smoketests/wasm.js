72bf79ba6b0458f24ed87250785d75881bef1987
Hans Ott <hansott@hotmail.be>
2025-02-07 12:38:04 +0100

    Remove shell injection detection algo (#76)
    * Remove shell injection detection algo
    
    Let's add back when it's 100%
    
    * Fix assertion in wasm smoke test
    
    * Add dialect

--- smoketests/wasm.js
+++ smoketests/wasm.js
@@ -3,7 +3,7 @@ const { equal } = require("node:assert");
 
 equal(internals.wasm_detect_sql_injection("SELECT * FROM users WHERE id = '' OR 1=1 -- '", "' OR 1=1 -- ", 0), true);
 
-equal(internals.wasm_detect_shell_injection("SELECT * FROM users WHERE id = 'hello world'", 'hello world'), false);
+equal(internals.wasm_detect_sql_injection("SELECT * FROM users WHERE id = 'hello world'", 'hello world'), false);
 
 equal(internals.wasm_detect_js_injection("const test = 'Hello World!'; //';", "Hello World!'; //", 0), true);
 
