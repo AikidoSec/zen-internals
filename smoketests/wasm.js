ffa7de5b7bd45772b53122d6cb0e40e05d8763a4
Hans Ott <hansott@hotmail.be>
2025-05-23 18:04:32 +0200

    Return 3 if tokenize error in wasm

--- smoketests/wasm.js
+++ smoketests/wasm.js
@@ -3,8 +3,11 @@ const { deepStrictEqual } = require("node:assert");
 const test = require("node:test");
 
 test("wasm_detect_sql_injection", () => {
- deepStrictEqual(internals.wasm_detect_sql_injection("SELECT * FROM users WHERE id = '' OR 1=1 -- '", "' OR 1=1 -- ", 0), true);
- deepStrictEqual(internals.wasm_detect_sql_injection("SELECT * FROM users WHERE id = 'hello world'", 'hello world'), false);
+ deepStrictEqual(internals.wasm_detect_sql_injection("SELECT * FROM users WHERE id = '' OR 1=1 -- '", "' OR 1=1 -- ", 0), 1);
+ deepStrictEqual(internals.wasm_detect_sql_injection("SELECT * FROM users WHERE id = 'hello world'", 'hello world'), 0);
+
+ // Tokenize error
+ deepStrictEqual(internals.wasm_detect_sql_injection("SELECT * FROM yo WHERE id = '", 'yo'), 3);
 });
 
 test("wasm_detect_js_injection", () => {
--- src/wasm_bindings/mod.rs
+++ src/wasm_bindings/mod.rs
@@ -1,10 +1,21 @@
 use crate::js_injection::detect_js_injection::detect_js_injection_str;
-use crate::sql_injection::detect_sql_injection::detect_sql_injection_str;
+use crate::sql_injection::detect_sql_injection::{detect_sql_injection_str, DetectionReason};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn wasm_detect_sql_injection(query: &str, userinput: &str, dialect: i32) -> bool {
-    detect_sql_injection_str(query, userinput, dialect).detected
+pub fn wasm_detect_sql_injection(query: &str, userinput: &str, dialect: i32) -> i32 {
+    let detection_results = detect_sql_injection_str(query, userinput, dialect);
+
+    if let DetectionReason::FailedToTokenizeQuery = detection_results.reason {
+        // make a special exception for failing to tokenize query (report code 3)
+        return 3;
+    }
+
+    if detection_results.detected {
+        1
+    } else {
+        0
+    }
 }
 
 #[wasm_bindgen]
