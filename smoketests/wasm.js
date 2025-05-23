000039983a70e7fb57e54b6f69f5e75f4aec07dd
Hans Ott <hansott@hotmail.be>
2025-05-23 18:02:27 +0200

    Use deepStrictEqual to verify output
    otherwise false is the same as 0

--- smoketests/wasm.js
+++ smoketests/wasm.js
@@ -1,10 +1,13 @@
 const internals = require("../pkg/zen_internals");
-const { equal } = require("node:assert");
+const { deepStrictEqual } = require("node:assert");
+const test = require("node:test");
 
-equal(internals.wasm_detect_sql_injection("SELECT * FROM users WHERE id = '' OR 1=1 -- '", "' OR 1=1 -- ", 0), true);
+test("wasm_detect_sql_injection", () => {
+ deepStrictEqual(internals.wasm_detect_sql_injection("SELECT * FROM users WHERE id = '' OR 1=1 -- '", "' OR 1=1 -- ", 0), true);
+ deepStrictEqual(internals.wasm_detect_sql_injection("SELECT * FROM users WHERE id = 'hello world'", 'hello world'), false);
+});
 
-equal(internals.wasm_detect_sql_injection("SELECT * FROM users WHERE id = 'hello world'", 'hello world'), false);
-
-equal(internals.wasm_detect_js_injection("const test = 'Hello World!'; //';", "Hello World!'; //", 0), true);
-
-equal(internals.wasm_detect_js_injection("const test = 'Hello World!'; //';", "Hello World!", 0), false);
+test("wasm_detect_js_injection", () => {
+ deepStrictEqual(internals.wasm_detect_js_injection("const test = 'Hello World!'; //';", "Hello World!'; //", 0), true);
+ deepStrictEqual(internals.wasm_detect_js_injection("const test = 'Hello World!'; //';", "Hello World!", 0), false);
+});
